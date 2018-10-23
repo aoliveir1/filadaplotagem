73f87e5268d66cd12fb75b298f62af63e2d88401
André <oioi-skin84@hotmail.com>
2018-10-23 15:33:38 -0300

    Update fila.py

--- fila.py
+++ fila.py
@@ -19,13 +19,12 @@ def get_pendentes():
     
     while True:
         browser = Browser('chrome', headless = True)
-        browser.visit(URL_LOGIN)
-        browser.fill('username', USERNAME)
-        browser.fill('password', PASSWORD)
+        browser.visit($URL_LOGIN)
+        browser.fill('username', $USERNAME)
+        browser.fill('password', $PASSWORD)
         browser.find_by_name('login_copista').click()
-        browser.visit(URL_PENDENTES)
-        print(browser.html)
-
+        browser.visit($URL_PENDENTES)
+ 
         if 'Nenhum protocolo encontrado.' not in browser.html:
             soup = BeautifulSoup(browser.html, 'html.parser')
             plots = soup.find_all('div', attrs={'class': 'titulo'})
