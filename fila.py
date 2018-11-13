bfdd2660e00e4e2e570ac7d74caac094c818d0d0
André <oioi-skin84@hotmail.com>
2018-11-13 16:39:05 -0200

    Update fila.py

--- fila.py
+++ fila.py
@@ -19,58 +19,58 @@ def get_pendentes():
     global atualizado
     global temp
       
-    while True:    
-        browser = Browser('chrome', headless = True)
-        browser.visit(os.environ.get('URL_LOGIN'))
-        browser.fill('username', os.environ.get('USERNAME'))
-        browser.fill('password', os.environ.get('PASSWORD'))
-        browser.find_by_name('login_copista').click()
-        browser.visit(os.environ.get('URL_PENDENTES'))
+    browser = Browser('chrome', headless = True)
+    browser.visit(os.environ.get('URL_LOGIN'))
+    browser.fill('username', os.environ.get('USERNAME'))
+    browser.fill('password', os.environ.get('PASSWORD'))
+    browser.find_by_name('login_copista').click()
+    browser.visit(os.environ.get('URL_PENDENTES'))
 
-        temp = []
-        if 'Nenhum protocolo encontrado.' not in browser.html:
-            soup = BeautifulSoup(browser.html, 'html.parser')
-            plots = soup.find_all('div', attrs={'class': 'titulo'})
+    temp = []
+    if 'Nenhum protocolo encontrado.' not in browser.html:
+        soup = BeautifulSoup(browser.html, 'html.parser')
+        plots = soup.find_all('div', attrs={'class': 'titulo'})
 
-            for i, plot in enumerate(plots):
-                if i > 0:
-                    plot = str(plot.text.strip())
-                    data = datetime.datetime.strptime(plot[:16].strip(), '%d/%m/%Y %H:%M').strftime('%d/%m/%Y %H:%M')
-                    protocolo = plot[21:31]
-                    try:
-                        #login = plot[36:].rstrip('_')
-                        pos = plot[36:].find('_')
-                        login = plot[36:36+pos]
-                        login = (login[:3]+'..'+login[len(login)-1:]).upper()
-                    except:
-                        login = 'nao-indentificado'
+        for i, plot in enumerate(plots):
+            if i > 0:
+                plot = str(plot.text.strip())
+                data = datetime.datetime.strptime(plot[:16].strip(), '%d/%m/%Y %H:%M').strftime('%d/%m/%Y %H:%M')
+                protocolo = plot[21:31]
+                try:
+                    #login = plot[36:].rstrip('_')
+                    pos = plot[36:].find('_')
+                    login = plot[36:36+pos]
+                    login = (login[:3]+'..'+login[len(login)-1:]).upper()
+                except:
+                    login = 'nao-indentificado'
 
-                    browser.visit(f'https://ucsvirtual.ucs.br/impressoes/plotista/{protocolo}')
-                    soup = BeautifulSoup(browser.html, 'html.parser')
-                    folha = soup.find_all('table')
-                    folha = str(folha)
-                    pos = folha.find('Tipo de folha:')                
-                    a = folha.find('A', pos)
-                    folha = folha[a:a+2]               
+                browser.visit(f'https://ucsvirtual.ucs.br/impressoes/plotista/{protocolo}')
+                soup = BeautifulSoup(browser.html, 'html.parser')
+                folha = soup.find_all('table')
+                folha = str(folha)
+                pos = folha.find('Tipo de folha:')                
+                a = folha.find('A', pos)
+                folha = folha[a:a+2]               
 
-                    plotagem = {'data': data, 'protocolo':protocolo, 'login':login, 'folha':folha}                    
-                    temp.append(plotagem)                    
+                plotagem = {'data': data, 'protocolo':protocolo, 'login':login, 'folha':folha}                    
+                temp.append(plotagem)                    
 
-        browser.quit()
+    browser.quit()
 
-        protocolos = temp 
-        atualizado = datetime.datetime.now(tz=tz).strftime('%d/%m/%Y %H:%M')
-
-        if datetime.datetime.now(tz=tz).hour >= 8 & datetime.datetime.now(tz=tz).hour <= 22:
-            time.sleep(60*5)
-        if datetime.datetime.now(tz=tz).hour >= 7 & datetime.datetime.now(tz=tz).hour <= 23:
-            time.sleep(60*10)
-        else:
-            time.sleep(60*20)
-            
+    protocolos = temp 
+    atualizado = datetime.datetime.now(tz=tz).strftime('%d/%m/%Y %H:%M')            
 
 t = threading.Thread(target=get_pendentes)
-t.start()
+
+while True:
+    t.start()    
+    if datetime.datetime.now(tz=tz).hour >= 8 & datetime.datetime.now(tz=tz).hour <= 22:
+        time.sleep(60*5)
+    if datetime.datetime.now(tz=tz).hour >= 7 & datetime.datetime.now(tz=tz).hour <= 23:
+        time.sleep(60*10)
+    else:
+        time.sleep(60*20)
+    t.stop()
 
 @route('/', 'GET')
 def pendentes():
